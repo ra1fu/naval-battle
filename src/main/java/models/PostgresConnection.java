cf91259560e6989f38579d92b5ef6c1ba36915fb
Rauan <kuntuganovrauan18@gmail.com>
2025-01-23 17:44:50 +0500

    maven

---
+++ src/main/java/models/PostgresConnection.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresConnection {
+
+    private static final String URL = "jdbc:postgresql://34.118.52.174:5432/naval"; // Замените на ваш URL
+    private static final String USER = "rauan"; // Ваш логин
+    private static final String PASSWORD = "0000"; // Ваш пароль
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
